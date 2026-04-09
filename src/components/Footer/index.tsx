c28e24a13d35d2b3b19d2dee30d2512d96856370
thomas1022-online <thomasvoliveira21@gmail.com>
2026-04-08 22:04:20 -0300

    comecei a realizar o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,31 @@
-function Footer(){
+function Footer() {
 
-    return(
+    return (
         <>
             <p>TO FUNCIONANDO</p>
+
+            <footer className="princ_1">
+
+                <div className="div_footer1">
+                    <p> Nome/frase</p>
+                </div>
+
+                <div className="div_footer2">
+                    <p>sobre
+                        <br />
+                        trabalhe conosco
+                        <br />
+                        suporte</p>
+                </div>
+                
+                <div className="div_footer3">
+                    <p>Campeonatos
+                        <br />
+                        Noticias</p>
+                </div>
+            </footer>
         </>
+
     )
 }
 
