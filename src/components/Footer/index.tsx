b7f2b29a7ac1fd9e3740cb9c7fb588c229017dee
thomas1022-online <thomasvoliveira21@gmail.com>
2026-04-10 22:05:42 -0300

    alteração do footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,29 +1,35 @@
-import"../../style/footer.css"
+
 function Footer() {
 
     return (
         <>
             <p>TO FUNCIONANDO</p>
 
-            <footer className="princ_1">
-
-                <div className="div_footer1">
-                    <p> Nome/frase</p>
-                </div>
-
-                <div className="div_footer2">
-                    <p>sobre
-                        <br />
-                        trabalhe conosco
-                        <br />
-                        suporte</p>
-                </div>
-
-                <div className="div_footer3">
-                    <p>Campeonatos
-                        <br />
-                        Noticias</p>
-                        
+              <footer>
+                <div className="footer-container">
+ 
+                    <div className="footer-col">
+                        <h3>Nome da Empresa</h3>
+                        <p>Uma frase curta que defina o propósito do seu projeto ou marca.</p>
+                    </div>
+ 
+                    <div className="footer-col">
+                        <h3>Institucional</h3>
+                        <ul>
+                            <li><a href="/sobre">Sobre</a></li>
+                            <li><a href="/trabalhe-conosco">Trabalhe Conosco</a></li>
+                            <li><a href="/suporte">Suporte</a></li>
+                        </ul>
+                    </div>
+ 
+                    <div className="footer_1">
+                        <h3>Conteúdo</h3>
+                        <ul>
+                            <li><a href="/campeonatos">Campeonatos</a></li>
+                            <li><a href="/noticias">Notícias</a></li>
+                        </ul>
+                    </div>
+ 
                 </div>
             </footer>
         </>
