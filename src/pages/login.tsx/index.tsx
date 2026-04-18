94c75f40f1300918e34d3223e5eda8564d5a552a
Thomas veneziano <thomasvoliveira21@gmail.com>
2026-04-18 16:44:34 -0300

    validação do formulario

--- src/pages/login.tsx/index.tsx
+++ src/pages/login.tsx/index.tsx
@@ -4,26 +4,33 @@ import "../../style/login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
-  
- 
-return (
-    <>
-        <Header />
 
-        <h1>Login</h1>
-        <div className="container_input">
-            <input type="email" placeholder="Email" className="input_login" />
-            <input type="password" placeholder="Senha" className="input_login" /><br /><br />
-            <button type="submit" className="button_login">Login</button><br />
-            <Link to="/register">Não tem uma conta? Cadastre-se aqui.</Link>
-        </div>
-        
 
-        <Footer />
+    return (
+        <>
+            <Header />
 
-    </>
+            <h1>Login</h1>
+            <div className="container_input">
+                <form action="registro">
+                    <label htmlFor="email"></label>
+                    <input type="email" placeholder="Email" className="input_login" />
+                    <label htmlFor="senha"></label>
+                    <input type="password" placeholder="Senha" className="input_login" /><br />
+                    <div className="login">
+                        <button type="submit" className="button_login">Login</button><br />
+                    </div>
+                </form>
+
+
+                <Link to="/register">Não tem uma conta? Cadastre-se aqui.</Link>
+            </div>
+
+
+            <Footer />
+
+        </>
     )
 }
 
 export default Login;
-    
