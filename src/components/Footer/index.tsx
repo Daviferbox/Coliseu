2c8b51ec7bf9ed8f54988d4ecceb69d46586719e
OliverDn95 <117095640+oliverdn95@users.noreply.github.com>
2026-05-13 21:59:44 -0300

    Criado estrutura da página 404 e corrigido links do footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import "../../style/footer.css"
 
 function Footer() {
+
+
+  const navigate = useNavigate();
 
   return (
     <>
@@ -15,17 +19,17 @@ function Footer() {
           <div className="footer__links">
             <h3 className="footer__title">Institucional</h3>
             <ul className="footer__list">
-              <li className="footer__item"><a href="/sobre" className="footer__link" data-desc="Saiba mais sobre nossa empresa e missão">Sobre</a></li>
-              <li className="footer__item"><a href="/trabalhe-conosco" className="footer__link" data-desc="Veja oportunidades de carreira">Trabalhe Conosco</a></li>
-              <li className="footer__item"><a href="/suporte" className="footer__link" data-desc="Central de ajuda e suporte ao usuário">Suporte</a></li>
+              <li className="footer__item"><a onClick={() =>navigate("/sobre")} className="footer__link" data-desc="Saiba mais sobre nossa empresa e missão">Sobre</a></li>
+              <li className="footer__item"><a onClick={() =>navigate("/trabalhe-conosco")} className="footer__link" data-desc="Veja oportunidades de carreira">Trabalhe Conosco</a></li>
+              <li className="footer__item"><a onClick={() =>navigate("/suporte")} className="footer__link" data-desc="Central de ajuda e suporte ao usuário">Suporte</a></li>
             </ul>
           </div>
 
           <div className="footer__links">
             <h3 className="footer__title">Conteúdo</h3>
             <ul className="footer__list">
-              <li className="footer__item"><a href="/campeonatos" className="footer__link" data-desc="Veja os campeonatos em destaque">Campeonatos</a></li>
-              <li className="footer__item"><a href="/noticias" className="footer__link" data-desc="Últimas notícias e atualizações">Notícias</a></li>
+              <li className="footer__item"><a onClick={() =>navigate("/campeonatos")} className="footer__link" data-desc="Veja os campeonatos em destaque">Campeonatos</a></li>
+              <li className="footer__item"><a onClick={() =>navigate("/noticias")} className="footer__link" data-desc="Últimas notícias e atualizações">Notícias</a></li>
             </ul>
           </div>
 
--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
 function NotFound(){
 
+    const navigate = useNavigate();
+    
     return(
         <>
+            <Header />
             <h1>Erro 404 Não encontrado</h1>
+            <p>A página que você está procurando não existe ou foi movida.</p>
+            <button onClick={() => navigate("/")} className="btn">Voltar para a página inicial</button>
+            <Footer />
         </>
     )
 }
